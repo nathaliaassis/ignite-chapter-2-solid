db25f2e351a3773459548f1a39ae232676481432
nathaliaassis <nathaliamassis@gmail.com>
2021-08-01 16:04:53 -0300

    test finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exist.");
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not admin.");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
